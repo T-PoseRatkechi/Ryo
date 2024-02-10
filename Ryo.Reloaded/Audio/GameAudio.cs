7c866d0c14ce6d82eb9c89a55cbd19d71cc8aeab
T-Pose Ratkechi <tposeratkechi@gmail.com>
2024-02-10 05:21:44 -0600

    Add default audio config for P3R BGM.

---
+++ Ryo.Reloaded/Audio/GameAudio.cs
@@ -0,0 +1,27 @@
+﻿using Ryo.Reloaded.CRI.Types;
+
+namespace Ryo.Reloaded.Audio;
+
+internal static class GameAudio
+{
+    private static readonly Dictionary<string, AudioConfig> defaults = new()
+    {
+        ["p3r"] = new()
+        {
+            Format = CRIATOM_FORMAT.ADX,
+            NumChannels = 2,
+            SampleRate = 44100,
+            CategoryIds = new int[] { 0, 13 },
+        },
+    };
+
+    public static AudioConfig GetDefaultConfig(string game)
+    {
+        if (defaults.TryGetValue(game, out var defaultConfig))
+        {
+            return defaultConfig;
+        }
+
+        return new();
+    }
+}
