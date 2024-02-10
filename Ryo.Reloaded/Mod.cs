86030ce249250eaa73577d8edf64e3b34911d08f
T-Pose Ratkechi <tposeratkechi@gmail.com>
2024-02-09 23:51:28 -0600

    Set up logging and config.

--- Ryo.Reloaded/Mod.cs
+++ Ryo.Reloaded/Mod.cs
@@ -2,43 +2,47 @@
 using Reloaded.Mod.Interfaces;
 using Ryo.Reloaded.Configuration;
 using Ryo.Reloaded.Template;
+using System.Drawing;
 
-namespace Ryo.Reloaded
+namespace Ryo.Reloaded;
+
+public class Mod : ModBase
 {
-    public class Mod : ModBase
+    private readonly IModLoader modLoader;
+    private readonly IReloadedHooks? hooks;
+    private readonly ILogger log;
+    private readonly IMod owner;
+
+    private Config config;
+    private readonly IModConfig modConfig;
+
+    public Mod(ModContext context)
     {
-        private readonly IModLoader modLoader;
-        private readonly IReloadedHooks? hooks;
-        private readonly ILogger log;
-        private readonly IMod owner;
+        this.modLoader = context.ModLoader;
+        this.hooks = context.Hooks;
+        this.log = context.Logger;
+        this.owner = context.Owner;
+        this.config = context.Configuration;
+        this.modConfig = context.ModConfig;
 
-        private Config config;
-        private readonly IModConfig modConfig;
-
-        public Mod(ModContext context)
-        {
-            this.modLoader = context.ModLoader;
-            this.hooks = context.Hooks;
-            this.log = context.Logger;
-            this.owner = context.Owner;
-            this.config = context.Configuration;
-            this.modConfig = context.ModConfig;
-        }
-
-        #region Standard Overrides
-        public override void ConfigurationUpdated(Config configuration)
-        {
-            // Apply settings from configuration.
-            // ... your code here.
-            config = configuration;
-            log.WriteLine($"[{modConfig.ModId}] Config Updated: Applying");
-        }
-        #endregion
-
-        #region For Exports, Serialization etc.
-#pragma warning disable CS8618 // Non-nullable field must contain a non-null value when exiting constructor. Consider declaring as nullable.
-        public Mod() { }
-#pragma warning restore CS8618
-        #endregion
+        Log.Initialize("Ryo", this.log, Color.FromArgb(138, 177, 255));
     }
+
+    #region Standard Overrides
+    public override void ConfigurationUpdated(Config configuration)
+    {
+        // Apply settings from configuration.
+        // ... your code here.
+        config = configuration;
+        log.WriteLine($"[{modConfig.ModId}] Config Updated: Applying");
+
+        Log.LogLevel = this.config.LogLevel;
+    }
+    #endregion
+
+    #region For Exports, Serialization etc.
+#pragma warning disable CS8618 // Non-nullable field must contain a non-null value when exiting constructor. Consider declaring as nullable.
+    public Mod() { }
+#pragma warning restore CS8618
+    #endregion
 }
