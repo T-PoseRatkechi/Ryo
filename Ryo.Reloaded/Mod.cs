d488f145244b9d5ea9c10e6298ae3ba0f8423672
T-Pose Ratkechi <tposeratkechi@gmail.com>
2024-02-10 05:23:32 -0600

    Load audio files from mods.

--- Ryo.Reloaded/Mod.cs
+++ Ryo.Reloaded/Mod.cs
@@ -1,9 +1,12 @@
 ﻿using Reloaded.Hooks.ReloadedII.Interfaces;
 using Reloaded.Memory.SigScan.ReloadedII.Interfaces;
 using Reloaded.Mod.Interfaces;
+using Reloaded.Mod.Interfaces.Internal;
+using Ryo.Reloaded.Audio;
 using Ryo.Reloaded.Configuration;
 using Ryo.Reloaded.CRI;
 using Ryo.Reloaded.Template;
+using System.Diagnostics;
 using System.Drawing;
 
 namespace Ryo.Reloaded;
@@ -18,7 +21,10 @@ public class Mod : ModBase
     private Config config;
     private readonly IModConfig modConfig;
 
+    private readonly string game;
     private readonly CriAtomEx criAtomEx;
+    private readonly AudioRegistry audioRegistry;
+    private readonly AudioService audioService;
 
     public Mod(ModContext context)
     {
@@ -29,13 +35,39 @@ public class Mod : ModBase
         this.config = context.Configuration;
         this.modConfig = context.ModConfig;
 
-        Log.Initialize("Ryo", this.log, Color.FromArgb(138, 177, 255));
+#if DEBUG
+        Debugger.Launch();
+#endif
 
+        this.game = Path.GetFileNameWithoutExtension(this.modLoader.GetAppConfig().AppId);
+
+        Log.Initialize($"Ryo ({this.game.ToUpper()})", this.log, Color.FromArgb(138, 177, 255));
         this.modLoader.GetController<IStartupScanner>().TryGetTarget(out var scanner);
 
-        this.criAtomEx = new(Path.GetFileNameWithoutExtension(this.modLoader.GetAppConfig().AppId));
+        this.criAtomEx = new(this.game);
         this.criAtomEx.Initialize(scanner!, this.hooks);
+
+        this.audioRegistry = new(this.game);
+        this.audioService = new(this.criAtomEx, this.audioRegistry);
+
+        this.modLoader.ModLoading += this.OnModLoading;
+
         this.ApplyConfig();
+    }
+
+    private void OnModLoading(IModV1 mod, IModConfigV1 config)
+    {
+        if (!config.ModDependencies.Contains(this.modConfig.ModId))
+        {
+            return;
+        }
+
+        var modDir = this.modLoader.GetDirectoryForModId(config.ModId);
+        var modAudioDir = Path.Join(modDir, "ryo", this.game);
+        if (Directory.Exists(modAudioDir))
+        {
+            this.audioRegistry.AddAudioFolder(modAudioDir);
+        }
     }
 
     private void ApplyConfig()
@@ -49,8 +81,8 @@ public class Mod : ModBase
     {
         // Apply settings from configuration.
         // ... your code here.
-        config = configuration;
-        log.WriteLine($"[{modConfig.ModId}] Config Updated: Applying");
+        this.config = configuration;
+        this.log.WriteLine($"[{this.modConfig.ModId}] Config Updated: Applying");
         this.ApplyConfig();
     }
     #endregion
