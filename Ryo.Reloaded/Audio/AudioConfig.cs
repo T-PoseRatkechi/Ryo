10abeff8663b7fd9a507ee7ed94506774a1e26c4
T-Pose Ratkechi <tposeratkechi@gmail.com>
2024-02-10 05:16:01 -0600

    Add audio config.

---
+++ Ryo.Reloaded/Audio/AudioConfig.cs
@@ -0,0 +1,20 @@
+﻿using Ryo.Reloaded.CRI.Types;
+
+namespace Ryo.Reloaded.Audio;
+
+internal class AudioConfig
+{
+    public string CueName { get; set; } = string.Empty;
+
+    public int SampleRate { get; set; } = 44100;
+
+    public CRIATOM_FORMAT Format { get; set; } = CRIATOM_FORMAT.ADX;
+
+    public int NumChannels { get; set; } = 2;
+
+    public int PlayerId { get; set; }
+
+    public int[] CategoryIds { get; set; } = Array.Empty<int>();
+
+    public string AudioFile { get; set; } = string.Empty;
+}
