d160c93f7e2af01d0a9185551fc78c18ba6e92eb
T-Pose Ratkechi <tposeratkechi@gmail.com>
2024-02-11 12:35:39 -0600

    If replacement audio uses different player, manually start playback.

--- Ryo.Reloaded/Audio/AudioService.cs
+++ Ryo.Reloaded/Audio/AudioService.cs
@@ -1,4 +1,5 @@
 ﻿using Reloaded.Hooks.Definitions;
+using Ryo.Interfaces.Types;
 using Ryo.Reloaded.CRI;
 using System.Runtime.InteropServices;
 using static Ryo.Reloaded.CRI.CriAtomExFunctions;
@@ -29,20 +30,33 @@ internal unsafe class AudioService
 
     private void CriAtomExPlayer_SetCueName(nint playerHn, nint acbHn, byte* cueName)
     {
-        var player = this.criAtomEx.GetPlayerByHn(playerHn);
+        var player = this.criAtomEx.GetPlayerByHn(playerHn)!;
         var cueNameStr = Marshal.PtrToStringAnsi((nint)cueName);
         if (this.audioRegistry.TryGetAudio(player, cueNameStr ?? string.Empty, out var audio))
         {
+            var manualStart = false;
+            if (player.Id != audio.PlayerId && this.criAtomEx.GetPlayerById(audio.PlayerId) is PlayerConfig newPlayer)
+            {
+                player = newPlayer;
+                manualStart = true;
+            }
+
             var audioData = this.GetAudioData(audio.AudioFile);
-            this.criAtomEx.Player_SetData(playerHn, (byte*)audioData.Buffer, audioData.Size);
-            this.criAtomEx.Player_SetFormat(playerHn, audio.Format);
-            this.criAtomEx.Player_SetSamplingRate(playerHn, audio.SampleRate);
-            this.criAtomEx.Player_SetNumChannels(playerHn, audio.NumChannels);
+            this.criAtomEx.Player_SetData(player.PlayerHn, (byte*)audioData.Buffer, audioData.Size);
+            this.criAtomEx.Player_SetFormat(player.PlayerHn, audio.Format);
+            this.criAtomEx.Player_SetSamplingRate(player.PlayerHn, audio.SampleRate);
+            this.criAtomEx.Player_SetNumChannels(player.PlayerHn, audio.NumChannels);
 
             // Apply categories.
             foreach (var id in audio.CategoryIds)
             {
-                this.criAtomEx.Player_SetCategoryById(playerHn, (uint)id);
+                this.criAtomEx.Player_SetCategoryById(player.PlayerHn, (uint)id);
+            }
+
+            if (manualStart)
+            {
+                this.criAtomEx.Player_Start(player.PlayerHn);
+                Log.Debug($"Manually started player with ID: {player.Id}");
             }
 
             Log.Debug($"Redirected Cue Name: {cueNameStr}\nFile: {audio.AudioFile}");
