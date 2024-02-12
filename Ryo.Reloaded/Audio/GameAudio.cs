856d5dcd8d051a5777e88dbddd3de1d7a67d1253
T-Pose Ratkechi <tposeratkechi@gmail.com>
2024-02-11 21:59:45 -0600

    Add system categories to default P3R config. Set default volume to better match P3R BGM. Create new instances when getting default config.

--- Ryo.Reloaded/Audio/GameAudio.cs
+++ Ryo.Reloaded/Audio/GameAudio.cs
@@ -4,23 +4,24 @@ namespace Ryo.Reloaded.Audio;
 
 internal static class GameAudio
 {
-    private static readonly Dictionary<string, AudioConfig> defaults = new()
+    private static readonly Dictionary<string, Func<AudioConfig>> defaults = new()
     {
-        ["p3r"] = new()
+        ["p3r"] = () => new()
         {
-            Format = CriAtom_Format.ADX,
+            Format = CriAtom_Format.HCA,
             NumChannels = 2,
             SampleRate = 44100,
-            CategoryIds = new int[] { 0, 13 },
+            CategoryIds = new int[] { 0, 3, 13, 18 },
             PlayerId = 0,
+            Volume = 0.20f,
         },
     };
 
-    public static AudioConfig GetDefaultConfig(string game)
+    public static AudioConfig CreateDefaultConfig(string game)
     {
         if (defaults.TryGetValue(game, out var defaultConfig))
         {
-            return defaultConfig;
+            return defaultConfig();
         }
 
         return new();
