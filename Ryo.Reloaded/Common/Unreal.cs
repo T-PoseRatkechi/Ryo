5f32a0a358a8c7687ad847fc4b5515b8687f8066
T-Pose Ratkechi <tposeratkechi@gmail.com>
2024-02-23 06:26:56 -0600

    Add common types.

---
+++ Ryo.Reloaded/Common/Unreal.cs
@@ -0,0 +1,22 @@
+﻿using System.Runtime.InteropServices;
+
+namespace Ryo.Reloaded.Common;
+
+[StructLayout(LayoutKind.Sequential, Size = 0x10)]
+public unsafe struct TArray<T> where T : unmanaged
+{
+    public T* AllocatorInstance;
+    public int Num;
+    public int Max;
+}
+
+[StructLayout(LayoutKind.Sequential, Size = 0x10)]
+public unsafe struct FString
+{
+    public TArray<ushort> Data;
+
+    public readonly string? GetString()
+    {
+        return Marshal.PtrToStringAuto((nint)Data.AllocatorInstance);
+    }
+}
