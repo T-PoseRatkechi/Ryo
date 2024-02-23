baaff41ffa1e41a5fff99cce77257ccaebe3aaf7
T-Pose Ratkechi <tposeratkechi@gmail.com>
2024-02-23 07:14:16 -0600

    Adjust volume hooks to use utils.

--- Ryo.Reloaded/P3R/VolumeGetterAlt.cs
+++ Ryo.Reloaded/P3R/VolumeGetterAlt.cs
@@ -1,13 +1,11 @@
-﻿using Reloaded.Hooks.Definitions;
-using Reloaded.Hooks.Definitions.X64;
-using Reloaded.Memory.SigScan.ReloadedII.Interfaces;
+﻿using Reloaded.Hooks.Definitions.X64;
 
 namespace Ryo.Reloaded.P3R;
 
 /// <summary>
 /// Re-add getting category volume by ID.
 /// </summary>
-internal class VolumeGetterAlt : IGameHook
+internal class VolumeGetterAlt
 {
     private GetCategoryIndexFromId? getCategoryIndex;
     private GetCategoryVolumeByIndex? getCategoryVolume;
@@ -18,10 +16,17 @@ internal class VolumeGetterAlt : IGameHook
     [Function(CallingConventions.Microsoft)]
     private delegate float GetCategoryVolumeByIndex(ushort index);
 
-    public void Initialize(IStartupScanner scanner, IReloadedHooks hooks)
+    public VolumeGetterAlt()
     {
-        scanner.Scan(nameof(GetCategoryVolumeByIndex), "40 53 48 83 EC 20 83 64 24 ?? 00", result => this.getCategoryVolume = hooks.CreateWrapper<GetCategoryVolumeByIndex>(result, out _));
-        scanner.Scan(nameof(GetCategoryIndexFromId), "48 89 5C 24 ?? 48 89 74 24 ?? 57 48 83 EC 20 8B F9 BE FF FF 00 00", result => this.getCategoryIndex = hooks.CreateWrapper<GetCategoryIndexFromId>(result, out _));
+        ScanHooks.Add(
+            nameof(GetCategoryVolumeByIndex),
+            "40 53 48 83 EC 20 83 64 24 ?? 00",
+            (hooks, result) => this.getCategoryVolume = hooks.CreateWrapper<GetCategoryVolumeByIndex>(result, out _));
+
+        ScanHooks.Add(
+            nameof(GetCategoryIndexFromId),
+            "48 89 5C 24 ?? 48 89 74 24 ?? 57 48 83 EC 20 8B F9 BE FF FF 00 00",
+            (hooks, result) => this.getCategoryIndex = hooks.CreateWrapper<GetCategoryIndexFromId>(result, out _));
     }
 
     public float CriAtomExCategory_GetVolumeById(uint id)
