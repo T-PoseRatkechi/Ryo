82d9b1dba17f6691a500011e5a136f901ee6956c
T-Pose Ratkechi <tposeratkechi@gmail.com>
2024-03-08 14:40:04 -0600

    Implement missing functions.

--- Ryo.Reloaded/RyoUtils.cs
+++ Ryo.Reloaded/RyoUtils.cs
@@ -5,6 +5,12 @@ namespace Ryo.Reloaded;
 
 internal class RyoUtils : IRyoUtils
 {
+    public nint GetAcbHn(string acbName)
+        => AcbRegistry.GetAcbHn(acbName);
+
     public string? GetAcbName(nint acbHn)
         => AcbRegistry.GetAcbName(acbHn);
+
+    public PlayerInfo GetPlayerInfo(nint playerHn)
+        => throw new NotImplementedException();
 }
