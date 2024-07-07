5a33fd055e5e611bba6e39bbe166a240e21fd1a2
T-Pose Ratkechi <tposeratkechi@gmail.com>
2024-07-06 19:28:04 -0500

    Move audio cache code to own class.

---
+++ Ryo.Reloaded/Audio/AudioCache.cs
@@ -0,0 +1,25 @@
+﻿using Ryo.Reloaded.Audio.Models;
+using System.Runtime.InteropServices;
+
+namespace Ryo.Reloaded.Audio;
+
+internal static class AudioCache
+{
+    private static readonly Dictionary<string, AudioData> cachedAudioData = new(StringComparer.OrdinalIgnoreCase);
+
+    public static AudioData GetAudioData(string audioFile)
+    {
+        if (cachedAudioData.TryGetValue(audioFile, out var existingData))
+        {
+            return existingData;
+        }
+
+        var data = File.ReadAllBytes(audioFile);
+        var buffer = Marshal.AllocHGlobal(data.Length);
+        Marshal.Copy(data, 0, buffer, data.Length);
+        cachedAudioData[audioFile] = new(buffer, data.Length);
+
+        Log.Debug($"Loading audio: {audioFile}");
+        return cachedAudioData[audioFile];
+    }
+}
